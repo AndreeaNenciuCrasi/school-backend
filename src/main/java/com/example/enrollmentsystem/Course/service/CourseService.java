40cc9acd69236927f37b3ebe09f44a89c18e8521
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2021-06-22 15:05:15 +0300

    adds save course method

--- src/main/java/com/example/enrollmentsystem/Course/service/CourseService.java
+++ src/main/java/com/example/enrollmentsystem/Course/service/CourseService.java
@@ -1,7 +1,19 @@
 package com.example.enrollmentsystem.Course.service;
 
+import com.example.enrollmentsystem.Course.model.Course;
+import com.example.enrollmentsystem.Course.repo.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CourseService {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    public Course addCourse(Course course){
+        return courseRepository.save(course);
+    }
+
+
 }
