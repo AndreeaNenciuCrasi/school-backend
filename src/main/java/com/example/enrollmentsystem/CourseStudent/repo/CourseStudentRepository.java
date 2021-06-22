606160cc12b516cba76e5b2e3f1025ad38ae63a8
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2021-06-22 13:31:15 +0300

    adds find by methods in course student repository

--- src/main/java/com/example/enrollmentsystem/CourseStudent/repo/CourseStudentRepository.java
+++ src/main/java/com/example/enrollmentsystem/CourseStudent/repo/CourseStudentRepository.java
@@ -3,5 +3,11 @@ package com.example.enrollmentsystem.CourseStudent.repo;
 import com.example.enrollmentsystem.CourseStudent.model.CourseStudent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseStudentRepository extends JpaRepository<CourseStudent, Long> {
+
+    List<CourseStudent> findByCourseTrainerId(Long trainerId);
+
+    List<CourseStudent> findByStudentId(Long studentId);
 }
