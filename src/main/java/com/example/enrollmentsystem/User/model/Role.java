bb6ef57740a48fefacad33e13dc0f9b6bbd73455
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2021-06-22 12:33:34 +0300

    first commit

---
+++ src/main/java/com/example/enrollmentsystem/User/model/Role.java
@@ -0,0 +1,7 @@
+package com.example.enrollmentsystem.User.model;
+
+public enum Role {
+    STUDENT,
+    TRAINER,
+    MANAGER
+}
---
+++ src/main/java/com/example/enrollmentsystem/User/model/User.java
@@ -0,0 +1,22 @@
+package com.example.enrollmentsystem.User.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String username;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
